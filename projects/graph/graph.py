bb1af27f47ce69305c83f611659555d12d65a389
melissa-murphy <melissamurphy.ca@gmail.com>
2020-05-11 13:03:44 -0700

    working part one of graph, day one

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,23 +3,24 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        """
-        Add a vertex to the graph.
-        """
-        pass  # TODO
+        #create set of vertices
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        pass  # TODO
+        #add edges from either side of vertex
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndentationError("Vertex does not exist in graph")
 
     def get_neighbors(self, vertex_id):
         """
@@ -75,6 +76,7 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
